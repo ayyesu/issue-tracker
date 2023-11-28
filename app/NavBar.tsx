fdd2f7a2e165f10042dc419b19a648ff040a2d5a
ayyesu <iamdanielkissiedu@gmail.com>
2023-11-28 23:54:52 +0000

    Styled active link in the navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,8 +1,14 @@
+'use client';
 import Link from 'next/link';
+import {usePathname} from 'next/navigation';
 import React from 'react';
 import {AiFillBug} from 'react-icons/ai';
+import classNames from 'classnames';
 
 const NavBar = () => {
+    const currentPath = usePathname();
+    console.log(currentPath);
+
     const links = [
         {
             label: 'Dashboard',
@@ -23,7 +29,11 @@ const NavBar = () => {
                 {links.map((link) => (
                     <Link
                         key={link.href}
-                        className='text-zinc-500 hover:text-zinc-800 transition-colors'
+                        className={classNames({
+                            'text-zinc-800': link.href === currentPath,
+                            'text-zinc-500': link.href !== currentPath,
+                            'hover:text-zinc-800 transition-colors': true,
+                        })}
                         href={link.href}
                     >
                         {link.label}
---
+++ app/issues/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const IssuePage = () => {
+    return <div>Issue Page</div>;
+};
+
+export default IssuePage;
