d2e4eb672df2f66ca03a359c2bb47da2d162b4c9
ayyesu <iamdanielkissiedu@gmail.com>
2023-11-29 14:55:13 +0000

    Add a spinner

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -10,6 +10,7 @@ import {zodResolver} from '@hookform/resolvers/zod';
 import {createIssueSchema} from '@/app/validationSchema';
 import {z} from 'zod';
 import ErrorMessage from '@/app/components/ErrorMessage';
+import Spinner from '@/app/components/Spinner';
 
 type IssueForm = z.infer<typeof createIssueSchema>;
 
@@ -24,6 +25,7 @@ const NewIssuePage = () => {
         resolver: zodResolver(createIssueSchema),
     });
     const [error, setError] = useState('');
+    const [submitting, setSubmitting] = useState(false);
 
     return (
         <div className='max-w-xl'>
@@ -37,9 +39,11 @@ const NewIssuePage = () => {
                 className='space-y-3'
                 onSubmit={handleSubmit(async (data) => {
                     try {
+                        setSubmitting(true);
                         await axios.post('/api/issues', data);
                         router.push('/issues');
                     } catch (error) {
+                        setSubmitting(false);
                         setError(
                             'An unexpected error occurred. Please try again.',
                         );
@@ -65,7 +69,9 @@ const NewIssuePage = () => {
 
                 <ErrorMessage>{errors.description?.message}</ErrorMessage>
 
-                <Button>Submit New Issue</Button>
+                <Button disabled={submitting}>
+                    Submit New Issue {submitting && <Spinner />}
+                </Button>
             </form>
         </div>
     );
