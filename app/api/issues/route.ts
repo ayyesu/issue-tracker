6cec803c69d3233bca3aab91168b6a4be1bf6b52
ayyesu <iamdanielkissiedu@gmail.com>
2023-11-29 10:34:00 +0000

    Build an API for creating issues

---
+++ app/api/issues/route.ts
@@ -0,0 +1,22 @@
+import {NextRequest, NextResponse} from 'next/server';
+import {z} from 'zod';
+import prisma from '@/prisma/client';
+
+const createIssueSchema = z.object({
+    title: z.string().min(3).max(255),
+    description: z.string().min(3),
+});
+
+export async function POST(request: NextRequest) {
+    const body = await request.json();
+    const validation = createIssueSchema.safeParse(body);
+
+    if (!validation.success)
+        return NextResponse.json(validation.error.errors, {status: 400});
+
+    const newIssue = await prisma.issue.create({
+        data: {title: body.title, description: body.description},
+    });
+
+    return NextResponse.json(newIssue, {status: 201});
+}
