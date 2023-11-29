afe7c0d8b6ebfa3078ab884d376f28dcc2ac5767
ayyesu <iamdanielkissiedu@gmail.com>
2023-11-29 16:49:15 +0000

    Show the issue

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,13 +1,49 @@
 import React from 'react';
-import {Button} from '@radix-ui/themes';
+import {Button, Table, TableColumnHeaderCell} from '@radix-ui/themes';
 import Link from 'next/link';
+import prisma from '@/prisma/client';
 
-const IssuePage = () => {
+const IssuePage = async () => {
+    const issues = await prisma.issue.findMany();
+
     return (
         <div>
-            <Button>
-                <Link href='/issues/new'>New Issue</Link>
-            </Button>
+            <div className='mb-5'>
+                <Button>
+                    <Link href='/issues/new'>New Issue</Link>
+                </Button>
+            </div>
+            <Table.Root variant='surface'>
+                <Table.Header>
+                    <Table.Row>
+                        <TableColumnHeaderCell>Issue</TableColumnHeaderCell>
+                        <TableColumnHeaderCell className='hidden md:table-cell'>
+                            Status
+                        </TableColumnHeaderCell>
+                        <TableColumnHeaderCell className='hidden md:table-cell'>
+                            Created
+                        </TableColumnHeaderCell>
+                    </Table.Row>
+                </Table.Header>
+                <Table.Body>
+                    {issues.map((issue) => (
+                        <Table.Row key={issue.id}>
+                            <Table.Cell>
+                                {issue.title}
+                                <div className='block md:hidden'>
+                                    {issue.status}
+                                </div>
+                            </Table.Cell>
+                            <Table.Cell className='hidden md:table-cell'>
+                                {issue.status}
+                            </Table.Cell>
+                            <Table.Cell className='hidden md:table-cell'>
+                                {issue.createdAt.toDateString()}
+                            </Table.Cell>
+                        </Table.Row>
+                    ))}
+                </Table.Body>
+            </Table.Root>
         </div>
     );
 };
