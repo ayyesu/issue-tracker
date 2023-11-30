24b1621d3ba318c9e10924f52dd1e97898775ec6
ayyesu <iamdanielkissiedu@gmail.com>
2023-11-30 01:55:24 +0000

    Add loading skeletons

--- app/issues/loading.tsx
+++ app/issues/loading.tsx
@@ -1,13 +1,11 @@
-import React from 'react';
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
 import {Table, TableColumnHeaderCell} from '@radix-ui/themes';
-import prisma from '@/prisma/client';
-import IssueStatusBadge from '../components/IssueStatusBadge';
-import delay from 'delay';
+import React from 'react';
 import IssueActions from './IssueActions';
 
-const IssuePage = async () => {
-    const issues = await prisma.issue.findMany();
-    await delay(2000);
+const LoadingIssuesPage = () => {
+    const issues = [1, 2, 3, 4, 5];
 
     return (
         <div>
@@ -26,18 +24,18 @@ const IssuePage = async () => {
                 </Table.Header>
                 <Table.Body>
                     {issues.map((issue) => (
-                        <Table.Row key={issue.id}>
+                        <Table.Row key={issue}>
                             <Table.Cell>
-                                {issue.title}
+                                <Skeleton />
                                 <div className='block md:hidden'>
-                                    <IssueStatusBadge status={issue.status} />
+                                    <Skeleton />
                                 </div>
                             </Table.Cell>
                             <Table.Cell className='hidden md:table-cell'>
-                                <IssueStatusBadge status={issue.status} />
+                                <Skeleton />
                             </Table.Cell>
                             <Table.Cell className='hidden md:table-cell'>
-                                {issue.createdAt.toDateString()}
+                                <Skeleton />
                             </Table.Cell>
                         </Table.Row>
                     ))}
@@ -47,4 +45,4 @@ const IssuePage = async () => {
     );
 };
 
-export default IssuePage;
+export default LoadingIssuesPage;
